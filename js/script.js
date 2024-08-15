6dbb9b7940cbcd3ed1756f93ee4242af36093b48
Nazar <example@gmail.com>
2024-08-15 17:34:03 +0300

    added style for window

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-import modal from './modules/modal';
+import modal from './modules/modal.js';
+import {openModal} from './modules/modal.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 500000);
           
-    
+
     modal('[data-modal]', '.modal', modalTimerId);
 });   
 
